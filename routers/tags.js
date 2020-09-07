d050a05a622d8116e24c18802301f0f9fad50559
sowjanyam27 <sowjanya.m94@gmail.com>
2020-09-07 09:51:58 +0200

    enabled .env for private keys

--- routers/tags.js
+++ routers/tags.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { Router } = require("express");
 const router = new Router();
 const { upload } = require("../utils");
@@ -5,17 +6,25 @@ const vision = require("@google-cloud/vision");
 const Image = require("../models").image;
 
 //Google Cloud Vision API
-const client = new vision.ImageAnnotatorClient({
+/* const client = new vision.ImageAnnotatorClient({
   keyFilename: "./APIKey.json",
+}); */
+
+const client = new vision.ImageAnnotatorClient({
+  projectId: process.env.GOOGLE_PROJECT_ID,
+  credentials: {
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  },
 });
 
 //Get all the Category
 router.post("/image", upload.single("image"), async (req, res, next) => {
   const { file } = req;
-  console.log("----file", file);
   try {
     // const labels = await analyseImage(file.path);
     // console.log("-----labels:", labels);
+    console.log("client:", client);
     const output = await client.labelDetection(file.path);
     const labels = output[0].labelAnnotations.map((label) => label.description);
     const newImage = await Image.create({
